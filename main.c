be87df96391c5930e542df438ff605ac2009dfdc
gerardo1878 <rdz1812@hotmail.com>
2019-08-30 09:35:47 -0500

    Practica4

--- main.c
+++ main.c
@@ -79,6 +79,7 @@ int main(int argc, char *argv[]) {
 	{
 		printf("condicion falsa\n");
 	}
+	if(C/B==-10)
 	{
 		printf("condicion verdadera\n");
 	}
@@ -90,10 +91,138 @@ int main(int argc, char *argv[]) {
 	{
 		printf("condicion verdadera\n");
 	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if((A+B==8) && (A-B==2))
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if((A+B == 8) || (A-B == 6))
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");	
+	}	
+	if(A>3 && B>3 && C<3)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
 	
-		
+	//************************************actividad 3***********************************
+
+	int a,b,c;
+	
+	printf("\n\nactividad 3\n");
+	printf("ingrese un valor para a\n");
+	scanf("%d", &a);
+	printf("ingrese un valor para b\n");
+	scanf("%d", &b);
+	printf("ingrese valores para c\n");
+	scanf("%d", &c);
+	
+	if(a>3)
+	{
+		printf("condicion verdadera\n");
+	}
+	else{
+		printf("condicion falsa\n");
+	}
+	if(a<c)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(b!=c)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(a==3)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(a*b==15)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(c/b<a)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(c/b==-10)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if(a+b+c==5)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if((a+b==8) && (a-b==2))
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
+	if((a+b == 8) || (a-b == 6))
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");	
+	}	
+	if(a>3 && b>3 && c<3)
+	{
+		printf("condicion verdadera\n");
+	}
+	else
+	{
+		printf("condicion falsa\n");
+	}
 	
 	
+
+
 	return 0;
 }
    
